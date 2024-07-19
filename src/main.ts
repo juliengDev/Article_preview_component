ce3a4f9ed6fd4e19313d4ae19a9b4546bb0533a6
Julien Gilbert <juliengilbert.dev@gmail.com>
2024-07-19 16:39:13 +0700

    HTML done

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,24 @@ import "../src/home";
 
 const btnShareEl = document.querySelector<HTMLButtonElement>(".btn__share");
 const socialLinksEl = document.querySelector<HTMLDivElement>(".social");
+const defaultFillColor = "#6E8098";
+const activeFillColor = "#FFF";
 
-btnShareEl?.addEventListener("click", () => {
-  if (socialLinksEl) socialLinksEl.classList.toggle("social-visible");
-});
+if (btnShareEl && socialLinksEl) {
+  const btnArrowEl = btnShareEl.querySelector<SVGPathElement>("path");
+
+  btnShareEl.addEventListener("click", () => {
+    socialLinksEl.classList.toggle("social-visible");
+    btnShareEl.classList.toggle("btn__share-active");
+
+    if (btnArrowEl) {
+      const isActive = btnShareEl.classList.contains("btn__share-active");
+      btnArrowEl.setAttribute(
+        "fill",
+        isActive ? activeFillColor : defaultFillColor
+      );
+    }
+  });
+} else {
+  console.error("Required elements not found");
+}
